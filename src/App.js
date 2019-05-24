f155619d7a1791d8644dce89f0c12d028072f75f
Juan Mateo Garcia Ramirez <juanmateo.garcia@globant.com>
2019-05-24 16:35:18 -0500

    Upload README and Layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./components/Layout";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/**
+  GraphQL Server: https://yoga-got.herokuapp.com
+**/
+
+const App = () => (
+    <Layout>
+      <h1 className="main-title">A story of ice and GraphQL</h1>
+    </Layout>
+);
 
 export default App;
