e9e55e6a7a5275ebba1f4a3dbcdf895e6451afbe
Juan Mateo Garcia Ramirez <juanmateo.garcia@globant.com>
2019-05-24 11:20:25 -0500

    Upload new README, and add new comments

--- src/_App.js
+++ src/_App.js
@@ -16,27 +16,34 @@ import "./App.css";
 /* import ExerciseThree from "./components/exerciseThree/exerciseThree"; */
 import ExerciseFour from "./components/exerciseFour/exerciseFour";
 
+/*
+  Apollo Boost Config
+
+  Para configurar React Apollo necesitaras de:
+  - Un provider que envuelva toda tu aplicacion en un contexto {  ApolloProvider }
+  - Un cliente que permite instanciar la configuracion de ApolloClient
+  - Instancia un nuevo cliente pasandole al constructor un objeto con las configuraciones que requieras
+  - En este caso solo necesitaremos pasarle un uri que corresponde a la dirección de nuestro servidor GraphQL
+*/
+
 /**
   GraphQL Server: https://yoga-got.herokuapp.com
 **/
 
 /* Apollo Boost Config */
-import { ApolloProvider } from "react-apollo";
+import {} from "react-apollo";
 import ApolloClient from "apollo-boost";
 
-/* Create the Apollo Client */
-const client = new ApolloClient({
-  uri: "https://yoga-got.herokuapp.com/"
-});
+/* Create the Apollo Client replace null with your ApollOClient instance */
+const client = null;
 
 const App = () => (
-  <ApolloProvider client={client}>
-    <Layout>
-      <h1 className="main-title">A story of ice and GraphQL</h1>
-      {/* Put your current component here! */}
-      <ExerciseFour />
-    </Layout>
-  </ApolloProvider>
+  /* Wrap all in a ApolloProvider context*/
+  <Layout>
+    <h1 className="main-title">A story of ice and GraphQL</h1>
+    {/* Put your current component here! */}
+    <ExerciseFour />
+  </Layout>
 );
 
 export default App;
